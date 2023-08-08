d20d16471dfda38bdea3b4e9c4a718352de458ee
Charan <r.charan.m.gm@gmail.com>
2023-08-08 18:12:29 +0530

    third commit

--- server.js
+++ server.js
@@ -12,24 +12,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
-   res.render("./home");
+//    res.render("./home");
+   res.sendFile(path.join(__dirname + '/home.ejs'));
 });
 app.get('/upload', function(req, res){
-    res.render("./upload");
+    // res.render("./upload");
+    res.sendFile(path.join(__dirname + '/upload.ejs'));
 });
 app.get('/status', function(req, res){
-    res.render("./status");
+    // res.render("./status");
+    res.sendFile(path.join(__dirname + '/status.ejs'));
 });
 app.get('/help', function(req, res){
-    res.render("./help");
+    // res.render("./help");
+    res.sendFile(path.join(__dirname + '/help.ejs'));
 });
 app.get('/contact', function(req, res){
-    res.render("./contact");
+    // res.render("./contact");
+    res.sendFile(path.join(__dirname + '/contact.ejs'));
 });
 app.get('/about', function(req, res){
-    res.render("./about");
+    // res.render("./about");
+    res.sendFile(path.join(__dirname + '/about.ejs'));
 });
-
+app.get('/names.txt', function(req, res){
+    res.sendFile(path.join(__dirname + '/images/names.txt'));
+});
+app.get('/script.js', function(req, res){
+    res.sendFile(path.join(__dirname + '/scripts/script.js'));
+});
+app.get('/styles.css', function(req, res){
+    res.sendFile(path.join(__dirname + '/scripts/styles.css'));
+});
 // app.listen(3000, function(){
 //     console.log("Started the Server at Port 3000.");
 // })
